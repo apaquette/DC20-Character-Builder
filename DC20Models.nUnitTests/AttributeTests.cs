64483f955bc28296c39d5ababa586112865b3c5d
Alex <alexandre.d.paquette+github@gmail.com>
2024-06-21 17:26:50 +0100

    Added Attribute Save tests

--- DC20Models.nUnitTests/AttributeTests.cs
+++ DC20Models.nUnitTests/AttributeTests.cs
@@ -8,6 +8,12 @@ public class AttributeTests {
     [TestCase(1, false, 1)]
     public void Attribute_ValueTest(int expected, bool save, int value) {
         Attribute attribute = new(() => _character.CombatMastery, value, save);
-        Assert.That(attribute.Value, Is.EqualTo(expected));
+        Assert.That((int)attribute, Is.EqualTo(expected));
+    }
+
+    [TestCase(1, false, 1)]
+    [TestCase(2, true, 1)]
+    public void Attribute_SaveTest(int expected, bool save, int value) {
+
     }
 }
