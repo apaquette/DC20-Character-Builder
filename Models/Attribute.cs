3ed30337fcdf63199ba9e0c54fbd89a3e5c96d1e
Alex <alexandre.d.paquette+github@gmail.com>
2024-06-21 17:19:07 +0100

    Added attribute class

---
+++ Models/Attribute.cs
@@ -0,0 +1,18 @@
+﻿namespace DC20Models;
+public class Attribute {
+    private readonly Func<int> _combatMastery;
+
+    public Attribute(Func<int> combatMastery, int value, bool saveProficiency) {
+        _combatMastery = combatMastery;
+        Value = value;
+        SaveProficiency = saveProficiency;
+    }
+
+    public int Value { get; private set; }
+    public int Save => SaveProficiency ? Value + _combatMastery() : Value;
+    public bool SaveProficiency { get; private set; }
+
+    public static implicit operator int(Attribute attribute) {
+        return attribute.Value;
+    }
+}
