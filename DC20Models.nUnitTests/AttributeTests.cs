cd5510b2b9e21a0673050cadd2b921c497ee02b3
Alex <alexandre.d.paquette+github@gmail.com>
2024-06-21 17:18:30 +0100

    Added tests for attribute

---
+++ DC20Models.nUnitTests/AttributeTests.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+
+namespace DC20Models.nUnitTests; 
+public class AttributeTests {
+    private Attribute? _attribute = null;
+    private Character? _character = new Character();
+
+    [TestCase(1, false, 1)]
+    public void Attribute_ValueTest(int expected, bool save, int value) {
+        Attribute attribute = new(() => _character.CombatMastery, value, save);
+        Assert.That(attribute.Value, Is.EqualTo(expected));
+    }
+}
--- DC20Models.nUnitTests/obj/Debug/net8.0/DC20Models.nUnitTests.AssemblyInfo.cs
+++ DC20Models.nUnitTests/obj/Debug/net8.0/DC20Models.nUnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DC20Models.nUnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ede064a26a3966c5e6bfcc2da0df64d39722523")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ba307b743141dcef110c7c6dde1dc2d6b91dc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DC20Models.nUnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DC20Models.nUnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
