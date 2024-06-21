70799996666b1268204200d78f50a84989e569b4
Alex <alexandre.d.paquette+github@gmail.com>
2024-06-21 20:44:15 +0100

    Test: Added additional unit tests for attribute value testing. Tested higher levels valid and invalid values in relation to attribute limit

--- DC20Models.nUnitTests/AttributeTests.cs
+++ DC20Models.nUnitTests/AttributeTests.cs
@@ -5,6 +5,8 @@ public class AttributeTests {
     [TestCase(1, false, 1, 1)]
     [TestCase(-2, false, -2, 1)]
     [TestCase(3, false, 3, 1)]
+    [TestCase(4, false, 4, 5)]
+    [TestCase(7, false, 7, 20)]
     public void Attribute_ValueTest(int expected, bool save, int value, int level) {
         Character thecharacter = LevelUpTo(new(), level);
         Attribute attribute = new(() => thecharacter.CombatMastery, value, save, () => thecharacter.AttributeLimit);
@@ -14,6 +16,8 @@ public class AttributeTests {
 
     [TestCase(false, -3, 1)]
     [TestCase(false, 4, 1)]
+    [TestCase(false, 5, 5)]
+    [TestCase(false, 10, 20)]
     public void Attribute_ValueTest_Invalid(bool save, int value, int level) {
         Assert.Throws<InvalidAttributeException>(() => {
             Character thecharacter = LevelUpTo(new(), level);
