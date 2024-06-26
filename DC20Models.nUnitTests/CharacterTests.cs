020b172d8c2289f0c071a86bb515e7c14fc6ce4d
Alex <alexandre.d.paquette+github@gmail.com>
2024-06-26 01:07:26 +0100

    Refactor: Modified test case for prime attribute to use a valid attribute selection based on the game rules (using standard array)

--- DC20Models.nUnitTests/CharacterTests.cs
+++ DC20Models.nUnitTests/CharacterTests.cs
@@ -27,7 +27,7 @@ public class CharacterTests {
         Assert.That(character.AttributeLimit, Is.EqualTo(expected));
     }
 
-    [TestCase(3, 1, 3, 2, 2, 2)]
+    [TestCase(3, 1, 3, 1, 2, -2)]
     public void PrimeAttribute(int expected, int level, int might, int agi, int cha, int inte) {
         Character character = LevelUpTo(new(null, null, might, agi, cha, inte), level);
         Assert.That(character.Prime, Is.EqualTo(expected));
