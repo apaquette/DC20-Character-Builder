73750e5d540b85bad83751d6609ccd010e9b6cf7
Alex <alexandre.d.paquette+github@gmail.com>
2024-06-27 17:46:10 +0100

    Test: Create unit test for Barbarian class to return correct Health Point Bonus based on character level

---
+++ DC20Models.nUnitTests/BarbarianTests.cs
@@ -0,0 +1,19 @@
+﻿using Models.Classes;
+
+namespace DC20Models.nUnitTests;
+public class BarbarianTests {
+    [TestCase(1,1)]
+    [TestCase(2,2)]
+    [TestCase(3,3)]
+    [TestCase(4,4)]
+    [TestCase(5,5)]
+    [TestCase(6,6)]
+    [TestCase(7,7)]
+    [TestCase(8,8)]
+    [TestCase(9,9)]
+    [TestCase(10,10)]
+    public void BonusHealthPoints(int expected, int Level) {
+        Barbarian barbarian = new Barbarian();
+        Assert.That(barbarian.BonusHP, Is.EqualTo(expected));
+    }
+}
